4127332aeb59d34c0a8999940fd98e4357633a6c
Kirt Henrie <sandnap@gmail.com>
2020-08-07 13:51:21 -0600

    Initial dashboard and header component

---
+++ app/components/header.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+class Header < ViewComponent::Base
+  # def initialize(predicate:, path:, disabled: false, label: nil)
+  #   @predicate = predicate
+  #   @path = path
+  #   @disabled = disabled
+  #   @label = label
+  def initialize()
+  end
+
+  private
+
+  # attr_reader :predicate, :path, :disabled, :label
+
+  # def icon_class
+  #   if predicate
+  #     'fas fa-toggle-on'
+  #   else
+  #     'fas fa-toggle-off'
+  #   end
+  # end
+end
