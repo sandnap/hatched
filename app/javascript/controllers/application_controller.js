dc346c2fc8e459684e8bc38f3e5784b794824315
Kirt Henrie <sandnap@gmail.com>
2020-08-07 13:49:35 -0600

    Added additional stimulus reflex controller

---
+++ app/javascript/controllers/application_controller.js
@@ -0,0 +1,12 @@
+import { Controller } from 'stimulus'
+import StimulusReflex from 'stimulus_reflex'
+
+export default class extends Controller {
+  connect () {
+    StimulusReflex.register(this)
+  }
+
+  sayHi () {
+    console.log('Hello from the Application controller.')
+  }
+}
