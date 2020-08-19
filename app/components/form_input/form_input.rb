a3be859ecc9218b87b230d74d8420bd8a7f27a1b
Kirt Henrie <sandnap@gmail.com>
2020-08-19 16:55:42 -0600

    Worked on layout and forms

---
+++ app/components/form_input/form_input.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module FormInput
+  class FormInput < ViewComponent::Base
+  end
+end
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
 class DashboardController < ApplicationController
-  before_action :authenticate_user!
-
   def index; end
 end
